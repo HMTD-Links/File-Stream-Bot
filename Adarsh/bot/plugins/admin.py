e2cf5653adb4fce37c50605752991db57de425ea
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-25 12:16:17 +0530

    Update admin.py

--- Adarsh/bot/plugins/admin.py
+++ Adarsh/bot/plugins/admin.py
@@ -15,7 +15,7 @@ from pyrogram.types import Message
 db = Database(Var.DATABASE_URL, Var.name)
 Broadcast_IDs = {}
 
-@StreamBot.on_message(filters.command("users") & filters.private )
+@StreamBot.on_message(filters.command("stats") & filters.private )
 async def sts(c: Client, m: Message):
     user_id=m.from_user.id
     if user_id in Var.OWNER_ID:
